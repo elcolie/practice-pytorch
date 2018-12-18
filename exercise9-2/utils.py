df48b7f1bef0ff8d67a6d5534fff233149ef0741
Sarit Ritwirune <sarit@elcolie.com>
2018-12-19 01:30:47 +0700

    Exercise 9-2: Otto classifier WIP

---
+++ exercise9-2/utils.py
@@ -0,0 +1,11 @@
+class_map = {
+    'Class_1': 1,
+    'Class_2': 2,
+    'Class_3': 3,
+    'Class_4': 4,
+    'Class_5': 5,
+    'Class_6': 6,
+    'Class_7': 7,
+    'Class_8': 8,
+    'Class_9': 9,
+}
