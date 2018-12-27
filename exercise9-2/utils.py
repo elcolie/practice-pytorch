39265d554a99007ea903f8c4b1edc857c0efcd87
Sarit Ritwirune <sarit@elcolie.com>
2018-12-28 01:48:02 +0700

    Classifying on Otto product Add confusion matrix to see the faulty prediction

--- exercise9-2/utils.py
+++ exercise9-2/utils.py
@@ -1,11 +1,91 @@
-class_map = {
-    'Class_1': 1,
-    'Class_2': 2,
-    'Class_3': 3,
-    'Class_4': 4,
-    'Class_5': 5,
-    'Class_6': 6,
-    'Class_7': 7,
-    'Class_8': 8,
-    'Class_9': 9,
+import itertools
+
+import matplotlib.pyplot as plt
+import numpy as np
+import torch
+from sklearn.metrics import confusion_matrix
+
+y_test = []
+y_pred = []
+
+CLASS_MAP = {
+    'Class_1': 0,
+    'Class_2': 1,
+    'Class_3': 2,
+    'Class_4': 3,
+    'Class_5': 4,
+    'Class_6': 5,
+    'Class_7': 6,
+    'Class_8': 7,
+    'Class_9': 8,
 }
+
+
+def plot_confusion_matrix(cm, classes,
+                          normalize=False,
+                          title='Confusion matrix',
+                          cmap=plt.cm.Blues):
+    """
+    This function prints and plots the confusion matrix.
+    Normalization can be applied by setting `normalize=True`.
+    """
+    print(cm)
+    plt.figure(figsize=(10, 10))
+    plt.imshow(cm, interpolation='nearest', cmap=cmap)
+    plt.title(title)
+    plt.colorbar()
+    tick_marks = np.arange(len(classes))
+    plt.xticks(tick_marks, classes, rotation=45)
+    plt.yticks(tick_marks, classes)
+
+    fmt = '.2f' if normalize else 'd'
+    thresh = cm.max() / 2.
+    for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
+        plt.text(j, i, format(cm[i, j], fmt),
+                 horizontalalignment="center",
+                 color="white" if cm[i, j] > thresh else "black")
+
+    plt.ylabel('True label')
+    plt.xlabel('Predicted label')
+    plt.tight_layout()
+
+
+def train(model, device, train_loader, optimizer, epoch, criterion):
+    model.train()
+    for batch_idx, (data, target) in enumerate(train_loader):
+        data, target = data.to(device), target.to(device)
+        optimizer.zero_grad()
+        output = model(data)
+        loss = criterion(output, target)
+        loss.backward()
+        optimizer.step()
+        if batch_idx % 100 == 0:
+            print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
+                epoch, batch_idx * len(data), len(train_loader.dataset),
+                       100. * batch_idx / len(train_loader), loss.item()))
+
+
+def test(model, device, test_loader, criterion):
+    model.eval()
+    test_loss = 0
+    correct = 0
+    with torch.no_grad():
+        for data, target in test_loader:
+            data, target = data.to(device), target.to(device)
+            output = model(data)
+            test_loss += criterion(output, target).item()  # sum up batch loss
+            pred = output.max(1, keepdim=True)[1]  # get the index of the max log-probability
+            y_test.append(int(target))
+            y_pred.append(int(pred))
+            correct += pred.eq(target.view_as(pred)).sum().item()
+
+    test_loss /= len(test_loader.dataset)
+
+    print('\nTest set: Average loss: {:.4f}, Accuracy: {}/{} ({:.0f}%)\n'.format(
+        test_loss, correct, len(test_loader.dataset),
+        100. * correct / len(test_loader.dataset)))
+
+    # Confusion matrix
+    confusion_mtx = confusion_matrix(y_test, y_pred)
+    plot_confusion_matrix(confusion_mtx, classes=[i for i in range(1, 9 + 1)], normalize=True,
+                          title='Normalized confusion matrix')
