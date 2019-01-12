d54611427f0d2e462ed2bdcda4113791294f6b23
Sarit Ritwirune <sarit.r@hbot.io>
2019-01-13 00:23:28 +0700

    Create `utils` file

---
+++ exercise13/utils.py
@@ -0,0 +1,110 @@
+import pandas as pd
+import torch
+import torch.utils.data
+import torchvision
+from torch.utils.data import Dataset
+
+
+class ImbalancedDatasetSampler(torch.utils.data.sampler.Sampler):
+    """Samples elements randomly from a given list of indices for imbalanced dataset
+    Arguments:
+        indices (list, optional): a list of indices
+        num_samples (int, optional): number of samples to draw
+    """
+
+    def __init__(self, dataset, indices=None, num_samples=None):
+
+        # if indices is not provided,
+        # all elements in the dataset will be considered
+        self.indices = list(range(len(dataset))) \
+            if indices is None else indices
+
+        # if num_samples is not provided,
+        # draw `len(indices)` samples in each iteration
+        self.num_samples = len(self.indices) \
+            if num_samples is None else num_samples
+
+        # distribution of classes in the dataset
+        label_to_count = {}
+        for idx in self.indices:
+            label = self._get_label(dataset, idx)
+            if label in label_to_count:
+                label_to_count[label] += 1
+            else:
+                label_to_count[label] = 1
+
+        # weight for each sample
+        weights = [1.0 / label_to_count[self._get_label(dataset, idx)]
+                   for idx in self.indices]
+        self.weights = torch.tensor(weights, dtype=torch.float)
+
+    def _get_label(self, dataset, idx):
+        dataset_type = type(dataset)
+        if dataset_type is torchvision.datasets.MNIST:
+            return dataset.train_labels[idx].item()
+        elif dataset_type is torchvision.datasets.ImageFolder:
+            return dataset.imgs[idx][1]
+        else:
+            return dataset[idx][1]
+
+    def __iter__(self):
+        return (self.indices[i] for i in torch.multinomial(
+            self.weights, self.num_samples, replacement=True))
+
+    def __len__(self):
+        return self.num_samples
+
+
+class Dictionary:
+    def __init__(self):
+        self.word2idx = {}
+        self.idx2word = {}
+        self.idx = 0
+
+    def add_word(self, word: str):
+        if word not in self.word2idx:
+            self.word2idx[word] = self.idx
+            self.idx2word[self.idx] = word
+            self.idx += 1
+
+    def __len__(self):
+        return len(self.word2idx)
+
+
+class WordDataset(Dataset):
+    def __init__(self, filename='train.tsv'):
+        super().__init__()
+        df = pd.read_csv(filename, sep='\t')
+        df['Phrase'] = df['Phrase'].apply(lambda x: x.lower())
+        df['tokenized'] = df['Phrase'].apply(lambda x: [i for i in x.split(' ')])
+
+        self.len = len(df)
+        self.df = df
+
+    def __getitem__(self, index):
+        record = self.df.iloc[index]
+        return record.tokenized, record.Sentiment
+
+    def __len__(self):
+        return self.len
+
+
+class RawDataset(Dataset):
+    """
+    Intentionally to use only
+    """
+
+    def __init__(self, df: pd.DataFrame):
+        super().__init__()
+        df['Phrase'] = df['Phrase'].apply(lambda x: x.lower())
+        df['tokenized'] = df['Phrase'].apply(lambda x: [i for i in x.split(' ')])
+
+        self.len = len(df)
+        self.df = df
+
+    def __getitem__(self, index):
+        record = self.df.iloc[index]
+        return record.tokenized, record.Sentiment
+
+    def __len__(self):
+        return self.len
