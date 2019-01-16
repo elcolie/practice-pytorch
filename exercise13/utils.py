3085103efe7e0220ed04d386f474dd89de8e82d2
Sarit Ritwirune <sarit.r@hbot.io>
2019-01-16 18:46:52 +0700

    Add `plot_confusion_matrix`

--- exercise13/utils.py
+++ exercise13/utils.py
@@ -1,3 +1,7 @@
+import itertools
+
+import matplotlib.pyplot as plt
+import numpy as np
 import pandas as pd
 import torch
 import torch.utils.data
@@ -87,3 +91,32 @@ class RawDataset(Dataset):
 
     def __len__(self):
         return self.len
+
+
+def plot_confusion_matrix(cm, classes,
+                          normalize=False,
+                          title='Confusion matrix',
+                          cmap=plt.cm.Blues):
+    """
+    This function prints and plots the confusion matrix.
+    Normalization can be applied by setting `normalize=True`.
+    """
+    print(cm)
+    plt.figure(figsize=(10, 10))
+    plt.imshow(cm, interpolation='nearest', cmap=cmap)
+    plt.title(title)
+    plt.colorbar()
+    tick_marks = np.arange(len(classes))
+    plt.xticks(tick_marks, classes, rotation=45)
+    plt.yticks(tick_marks, classes)
+
+    fmt = '.2f' if normalize else 'd'
+    thresh = cm.max() / 2.
+    for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
+        plt.text(j, i, format(cm[i, j], fmt),
+                 horizontalalignment="center",
+                 color="white" if cm[i, j] > thresh else "black")
+
+    plt.ylabel('True label')
+    plt.xlabel('Predicted label')
+    plt.tight_layout()
